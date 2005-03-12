797ecc4c398aefa7ff0d34da62b85a68542eca96
brian d foy <brian.d.foy@gmail.com>
2005-03-12 04:52:25 +0000

    * Added POD coverage tests and fixed up problems * updated  copyright info
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1611 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- SimpleLinkExtor.pm
+++ SimpleLinkExtor.pm
@@ -283,7 +283,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004 brian d foy.  All rights reserved.
+Copyright (c) 2004-2005 brian d foy.  All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
